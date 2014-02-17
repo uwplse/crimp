16aeff2ab63cd04c1257d0fc905bebb8fdcfd731
Kivanc Muslu <kivancmuslu@outlook.com>
2014-02-16 22:49:30 -0800

    Finally managed to extend the modelling to project (FOR, yay!), which made everything extremely complex :-). Of course the proof is broken. :-(

--- Relational2.v
+++ Relational2.v
@@ -1,35 +1,68 @@
 Require Import Bool Arith List CpdtTactics.
 Set Implicit Arguments.
 
+Inductive tuple : Set :=
+  | TCons : nat -> tuple -> tuple
+  | TNil : tuple.
+
 Inductive relation : Set :=
-  | Relation : relation
-  | Rnil : relation.
+  | RCons : tuple -> relation -> relation
+  | RNil : relation.
 
 Inductive Bool : Set :=
   | BTrue : Bool
   | BFalse : Bool.
 
 Inductive Query : Set := 
- | Select : Bool -> Query.
+  | Select : Bool -> Query
+  | Project : nat -> Query. 
 
-Definition runQuery (q : Query) (r : relation) : option relation :=
-  match q with 
-   | Select b => match b with 
-                  | BTrue => Some r
-                  | BFalse => Some Rnil
-                 end 
+Fixpoint projectTuple (t: tuple) (index: nat) : option tuple :=
+  match t with
+  | TNil => None
+  | TCons n rem => match index with
+                   | 0 => Some (TCons n TNil)
+                   | S index' => projectTuple rem index'
+                   end
   end.
 
+Definition runQuery (q : Query) (inputRelation : relation) : option relation :=
+  match q with 
+  | Select b => match b with 
+                | BTrue => Some inputRelation
+                | BFalse => Some RNil
+                end 
+  | Project index => 
+    let fix project (input: relation) (index: nat) (result: option relation) :=
+      match input with
+      | RNil => result
+      | RCons tup rem => match result with
+                         | None => None
+                         | Some resultRel => match (projectTuple tup index) with
+                                             | None => None
+                                             | Some tup' => project rem index (Some (RCons tup' resultRel))
+                                             end
+                         end
+      end
+    in project inputRelation index (Some RNil)
+  end.
+
+Inductive VarName : Set :=
+  | ResultName : VarName
+  | IndexedVarName : nat -> VarName.
 
 Inductive Exp : Set :=
   | InputRelation : Exp
-  | RelationExp : relation -> Exp.
+  | RelationExp : relation -> Exp
+(*  | TupleExp : tuple -> Exp *)
+  | NatExp : nat -> Exp
+  | ProjectTuple : Exp -> VarName -> Exp.
 
-Inductive VarName : Set :=
-  | ResultName : VarName.
-
+(* It turns out that Forall is already defined in Coq, so I used ForAll *)
 Inductive Statement : Set :=
-  | Assign : VarName -> Exp -> Statement.
+  | Assign : VarName -> Exp -> Statement
+  | AppendTuple: VarName -> Exp -> Statement
+  | ForAll : VarName -> Exp -> Statement -> Statement.
 
 Inductive ImpProgram : Set :=
   | Seq : Statement -> ImpProgram -> ImpProgram
@@ -37,20 +70,74 @@ Inductive ImpProgram : Set :=
 
 Definition queryToImp (q : Query) : option ImpProgram :=
   match q with
-   | Select b => match b with
-                   | BTrue => Some (Seq (Assign ResultName InputRelation) Skip) 
-                   | BFalse => Some (Seq (Assign ResultName (RelationExp Rnil)) Skip)
-                 end
+  | Select b => match b with
+                | BTrue => Some (Seq (Assign ResultName InputRelation) Skip) 
+                | BFalse => Some (Seq (Assign ResultName (RelationExp RNil)) Skip)   
+                end
+  | Project index => Some 
+                     (Seq 
+                      (Assign ResultName (RelationExp RNil))
+                      (Seq
+                        (ForAll (IndexedVarName 0) InputRelation
+                          (AppendTuple ResultName (ProjectTuple (NatExp index) (IndexedVarName 0))))
+                        Skip))
+                        
   end.
 
-Fixpoint runStatement (s: Statement) (input: relation) : option relation :=
-  match s with
-  | Assign vn e => match vn with
-                   | ResultName => match e with
-                                   | InputRelation => Some input
-                                   | RelationExp rexp => Some rexp 
-                                   end
+Fixpoint tupleHeapLookup (heap: relation) (index: nat) : option tuple :=
+  match heap with
+  | RNil => None
+  | RCons t rem => match index with
+                   | 0 => Some t
+                   | S index' => tupleHeapLookup rem index'
                    end
+  end.
+
+Fixpoint updateTupleHeap (heap: relation) (index: nat) (t: tuple) : relation :=
+  match heap with
+  | RNil => match index with
+            | 0 => RCons t RNil
+            | S index' => RCons (TCons 0 TNil) (updateTupleHeap heap index' t)
+            end
+  | RCons tup rem => match index with
+                     | 0 => RCons t rem
+                     | S index' => RCons tup (updateTupleHeap rem index' t)
+                     end
+  end.
+
+Fixpoint runStatement (s: Statement) (input: relation) (heap: relation) (result: relation) : option relation :=
+  match s with
+  | Assign ResultName e => match e with
+                           | InputRelation => Some input
+                           | RelationExp rexp => Some rexp 
+                           | _ => None
+                           end
+  | Assign _ _ => None
+  | AppendTuple ResultName e => 
+      match e with
+      | ProjectTuple (NatExp index) (IndexedVarName vnIndex) =>
+          match tupleHeapLookup heap vnIndex with
+          | Some tuple' => 
+              match projectTuple tuple' index with
+              | Some t' => Some (RCons t' result)
+              | None => None
+              end
+          | None => None
+          end
+      | _ => None
+      end
+  | AppendTuple _ _ => None
+  | ForAll (IndexedVarName index) InputRelation  s' =>
+      let fix helper (rel: relation) (res: option relation) :=
+        match res with
+        | None => None
+        | Some res' => match rel with
+                      | RNil => res
+                      | RCons t rem => helper rem (runStatement s' input (updateTupleHeap heap index t) res')
+                      end
+        end
+      in helper input (Some result)
+  | ForAll _ _ _ => None
   end.
 
 (* It turns out that we do not (and should not) have
@@ -63,9 +150,9 @@ Definition runImp (p : ImpProgram) (input : relation) : option relation :=
   let fix helper (p : ImpProgram) (result: option relation) : option relation := 
     match p with
     | Skip => result
-    | Seq s p' => helper p' (runStatement s input)
+    | Seq s p' => helper p' (runStatement s input RNil RNil)
     end
-  in helper p (Some Rnil).
+  in helper p (Some RNil).
 
 Theorem queryEquivalence: 
   forall (q : Query) (p : ImpProgram),
